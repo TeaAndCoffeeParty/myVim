fed3d97890b3e28698ccaf397a5fd7413137fb04
Chen Tuo <chentuo@sprintray.cn>
2024-08-05 13:27:39 +0800

    update the home path and directory checking

--- setUpVim.sh
+++ setUpVim.sh
@@ -5,7 +5,13 @@ sudo apt-get install -y vim exuberant-ctags python3-pip vim
 pip3 install --user pynvim
 pip3 install --user neovim
 
+if [ -d "$HOME/.vim/bundle/Vundle.vim" ];then 
+	echo "~/.vim/bundle/Vundle.vim has exists"
+else
+	git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+fi
 
-git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
 cp ./vimrc ~/.vimrc
+sed -i "s|/home/Tuo|$(echo $HOME)|g" ~/.vimrc
+
 vim +PluginInstall +qall
